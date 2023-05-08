1cb17eb7604327a9c753b53144db1e7c4a057552
BenThomson2007 <124379717+BenThomson2007@users.noreply.github.com>
2023-05-08 22:56:53 +0000

    Updated JS

--- js/script.js
+++ js/script.js
@@ -16,27 +16,36 @@ function NumbersGiven() {
   let minNumber = parseInt(document.getElementById("min-number").value);
   let maxNumber = parseInt(document.getElementById("max-number").value);
 
-  // Initializing the counter variable to the minimum number
-  let counter = minNumber;
-
-  // If statement to check whether minNumber > maxNumber (there is an error)
-  if (minNumber > maxNumber) {
-    message = "Please be sure that your minimum value is less than or equal to your maximum value.";
+  
+  // If the user does not enter their min and max, display that they must enter both numbers
+  if ((isNaN(minNumber)) || (isNaN(maxNumber))) {
+    message = "Please enter both a minimum and maximum number.";
   }
-    
-  // Else, complete the while loop to create list of the numbers between min and max
+
+  // Otherwise, continue the function to list the numbers
   else {
-    // While loop that specifies that the code within the loop executes as long as the counter is less than or equal to the maximum
-    while (counter <= maxNumber) {
-
-      // Building the string of numbers with a line break
-      numbers = numbers + counter + "<br>";
-
-      // Increment the counter with each number
-      counter = counter + 1;
-
-      // Set the message variable to display results
-      message = "Here are all the numbers between the minimum and maximum that you entered:<br><br>" + numbers;
+    // Initializing the counter variable to the minimum number
+    let counter = minNumber;
+  
+    // If statement to check whether minNumber > maxNumber (there is an error)
+    if (minNumber > maxNumber) {
+      message = "Please be sure that your minimum value is less than or equal to your maximum value.";
+    }
+      
+    // Else, complete the while loop to create list of the numbers between min and max
+    else {
+      // While loop that specifies that the code within the loop executes as long as the counter is less than or equal to the maximum
+      while (counter <= maxNumber) {
+  
+        // Building the string of numbers with a line break
+        numbers = numbers + counter + "<br>";
+  
+        // Increment the counter with each number
+        counter = counter + 1;
+  
+        // Set the message variable to display results
+        message = "Here are all the numbers between the minimum and maximum that you entered:<br><br>" + numbers;
+      }
     }
   }
 
